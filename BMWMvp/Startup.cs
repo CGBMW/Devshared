4e84f670bc19308e12cd097f019d63aa7769cbc2
Rajesh725 <kommireddy.rajesh@capgemini.com>
2017-04-24 17:03:31 +0530

    project commit

---
+++ BMWMvp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BMWMvp.Startup))]
+namespace BMWMvp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
